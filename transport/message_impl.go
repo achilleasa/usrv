230284a6e7c3c496a8630ebcad9b9b69f2398387
Achilleas Anagnostopoulos <achilleas.imap.mbox@gmail.com>
2016-12-14 15:26:38 +0000

    Implement generic transport message
    Other transports can use this implementation as the basis for their own
    message implementations.

---
+++ transport/message_impl.go
@@ -0,0 +1,116 @@
+package transport
+
+import (
+	"net/http"
+	"sync"
+
+	"github.com/google/uuid"
+)
+
+var (
+	// We allocate our messages of a pool so we can reduce GC pressure.
+	msgPool = sync.Pool{
+		New: func() interface{} {
+			return &GenericMessage{}
+		},
+	}
+)
+
+// GenericMessage provides a re-usable message implementation that implements both
+// Message and ImmutableMessage interfaces. Transports can use this as a basis
+// for their own message implementations.
+type GenericMessage struct {
+	IDField               string
+	SenderField           string
+	ReceiverField         string
+	SenderEndpointField   string
+	ReceiverEndpointField string
+	HeadersField          map[string]string
+	PayloadField          []byte
+	ErrField              error
+}
+
+// Returns a UUID for this message.
+func (m *GenericMessage) ID() string {
+	return m.IDField
+}
+
+// Close implements io.Closer and should be called when the message is no longer used.
+func (m *GenericMessage) Close() error {
+	m.PayloadField = nil
+	m.ErrField = nil
+	m.HeadersField = nil
+	msgPool.Put(m)
+
+	return nil
+}
+
+// Sender returns the name of the service that sent the mesage.
+func (m *GenericMessage) Sender() string {
+	return m.SenderField
+}
+
+// Receiver returns the name of the service that will receive the message.
+func (m *GenericMessage) Receiver() string {
+	return m.ReceiverField
+}
+
+// SenderEndpoint returns the sending service's endpoint.
+func (m *GenericMessage) SenderEndpoint() string {
+	return m.SenderEndpointField
+}
+
+// ReceiverEndpoint returns the receiving service's endpoint.
+func (m *GenericMessage) ReceiverEndpoint() string {
+	return m.ReceiverEndpointField
+}
+
+// Headers returns a map of header values associated with the message.
+func (m *GenericMessage) Headers() map[string]string {
+	return m.HeadersField
+}
+
+// SetHeader sets the content of a message header to the specified value.
+// If the specified header already exists, its value will be overwritten
+// by the new value.
+//
+// This function ensures that header names are always canonicalized by passing
+// them through http.CanonicalHeaderKey.
+func (m *GenericMessage) SetHeader(name, value string) {
+	m.HeadersField[http.CanonicalHeaderKey(name)] = value
+}
+
+// SetHeaders sets the contents of a batch of headers. This is equivalent
+// to iterating the map and calling SetHeader for each key/value.
+func (m *GenericMessage) SetHeaders(values map[string]string) {
+	for k, v := range values {
+		m.HeadersField[http.CanonicalHeaderKey(k)] = v
+	}
+}
+
+// Payload returns the payload associated with the message as a byte
+// slice or an error if one is encoded in the message.
+func (m *GenericMessage) Payload() ([]byte, error) {
+	return m.PayloadField, m.ErrField
+}
+
+// SetPayload sets the content of this message. The content may be either
+// a byte slice or an error message.
+func (m *GenericMessage) SetPayload(payload []byte, err error) {
+	m.PayloadField = payload
+	m.ErrField = err
+}
+
+// MakeGenericMessage creates a new GenericMessage instance
+func MakeGenericMessage() *GenericMessage {
+	m := msgPool.Get().(*GenericMessage)
+	m.IDField = GenerateID()
+	m.HeadersField = make(map[string]string, 0)
+
+	return m
+}
+
+// GenerateID generates a formatted V1 UUID that can be used as a message ID.
+func GenerateID() string {
+	return uuid.New().String()
+}
---
+++ transport/message_impl_test.go
@@ -0,0 +1,76 @@
+package transport
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestGenericMessage(t *testing.T) {
+	m := MakeGenericMessage()
+	var iface interface{} = m
+
+	if _, ok := iface.(ImmutableMessage); !ok {
+		t.Fatal("generic message instance does not implement ImmutableMessage")
+	}
+
+	if _, ok := iface.(Message); !ok {
+		t.Fatal("generic message instance does not implement Message")
+	}
+
+	expValue := GenerateID()
+	m.IDField = expValue
+	if m.ID() != expValue {
+		t.Errorf("expected ID() to return %q; got %q", expValue, m.ID())
+	}
+
+	expValue = "Sender"
+	m.SenderField = expValue
+	if m.Sender() != expValue {
+		t.Errorf("expected Sender() to return %q; got %q", expValue, m.Sender())
+	}
+
+	expValue = "SenderEndpoint"
+	m.SenderEndpointField = expValue
+	if m.SenderEndpoint() != expValue {
+		t.Errorf("expected SenderEndpoint() to return %q; got %q", expValue, m.SenderEndpoint())
+	}
+
+	expValue = "Receiver"
+	m.ReceiverField = expValue
+	if m.Receiver() != expValue {
+		t.Errorf("expected Receiver() to return %q; got %q", expValue, m.Receiver())
+	}
+
+	expValue = "ReceiverEndpoint"
+	m.ReceiverEndpointField = expValue
+	if m.ReceiverEndpoint() != expValue {
+		t.Errorf("expected ReceiverEndpoint() to return %q; got %q", expValue, m.ReceiverEndpoint())
+	}
+	expValue = "foo"
+	m.SetHeader("kEy1", expValue)
+	if m.Headers()["Key1"] != expValue {
+		t.Errorf(`expected Headers() to contain value ("key1", %q); got value %q`, expValue, m.Headers()["Key1"])
+	}
+
+	expHeaders := map[string]string{
+		"Key1": "value1",
+		"Key2": "value2",
+	}
+	m.SetHeaders(expHeaders)
+	if !reflect.DeepEqual(m.Headers(), expHeaders) {
+		t.Errorf("expected headers to be %v; got %v", expHeaders, m.Headers())
+	}
+
+	expPayload := []byte("hello")
+	m.SetPayload(expPayload, ErrNotAuthorized)
+
+	payload, err := m.Payload()
+	if !reflect.DeepEqual(payload, expPayload) {
+		t.Errorf("payload body mismatch")
+	}
+	if err != ErrNotAuthorized {
+		t.Errorf("payload error mismatch; expected %v; got %v", ErrNotAuthorized, err)
+	}
+
+	m.Close()
+}
