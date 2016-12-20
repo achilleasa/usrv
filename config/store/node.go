fbd088a1ac5a739336e7ab453d868526cf9e21a2
Achilleas Anagnostopoulos <achilleas.imap.mbox@gmail.com>
2016-12-20 21:35:19 +0000

    Implement config store node

---
+++ config/store/node.go
@@ -0,0 +1,187 @@
+package store
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+const (
+	pathDelimiter = "/"
+)
+
+// Node is used as the building block of a configuration tree. Nodes that
+// serve as the tree leaves store a string value while non-leaf nodes maintain
+// a map containing their child nodes.
+//
+// All nodes include a version value which is used when merging to preserve
+// or replace existing values.
+type node struct {
+	// The path segment where this node is located.
+	segment string
+
+	// The stored value for leaf nodes.
+	value string
+
+	// The version of the currently stored value.
+	version int
+
+	// The number of hops from the root of the configuration tree.
+	depth int
+
+	// The set of sub-paths originating from this segment. If this is a leaf
+	// node then the paths map length will be 0.
+	paths map[string]*node
+
+	// The parent node in this path.
+	parent *node
+}
+
+// Path returns the full path to this node. The full path consists of all segments
+// from the root of the tree up to this node separated by a '/' character.
+func (n *node) path() string {
+	segments := make([]string, n.depth)
+	index := n.depth - 1
+	for node := n; node.parent != nil; node = node.parent {
+		segments[index] = node.segment
+		index--
+	}
+
+	return pathDelimiter + strings.Join(segments, pathDelimiter)
+}
+
+// IsLeaf returns true if this is a leaf node.
+func (n *node) isLeaf() bool {
+	return len(n.paths) == 0
+}
+
+// LeafValues performs a DFS on a subtree rooted at the current node and populates
+// a map with the leaf values. The map keys are built by concatenating the supplied
+// path prefix and the full path from this node to each leaf.
+//
+// Given a node structure that looks like:
+//  {
+//   "key1": {
+//     "key2": "2",
+//     "key3": {
+//      "key4": "4"
+//     }
+//   }
+//  }
+//
+// Calling TreeValues on the root node with pathPrefix = "test/" and pathDelimiter
+// set to "/" will return:
+//  {
+//   "test/key1/key2": "2",
+//   "test/key1/key3/key4": "4"
+//  }
+func (n *node) leafValues(pathPrefix string, isRoot bool) map[string]string {
+	// If this is the subtree root and pathPrefix is not empty, ensure that
+	// it ends with a single pathDelimiter
+	if isRoot && len(pathPrefix) != 0 {
+		pathPrefix = strings.TrimRight(pathPrefix, pathDelimiter) + pathDelimiter
+	}
+
+	// This is a leaf
+	if n.isLeaf() {
+		return map[string]string{
+			pathPrefix + n.segment: n.value,
+		}
+	}
+
+	if !isRoot {
+		pathPrefix += n.segment + pathDelimiter
+	}
+
+	// Recursively build map by calling TreeValues on the child nodes and
+	// merging the maps into a single value map
+	valueMap := make(map[string]string, 0)
+	for _, subPathNode := range n.paths {
+		for k, v := range subPathNode.leafValues(pathPrefix, false) {
+			valueMap[k] = v
+		}
+	}
+
+	return valueMap
+}
+
+// Merge recursively attempts to apply the given value to this node and its children
+// updating leaf nodes for which the curently stored value version is less than or
+// equal to the supplied version.
+//
+// Merge accepts an interface for its value but in practice only supports
+// two types of values:
+//  - A string value which only gets applied if this node is a leaf after a version check
+//  - A map[string]interface{} value for which merge iterates its keys and propagates
+//    the values to child nodes, creating them if they do not exist.
+//
+// If the value argument does not match the expected values then merge will panic.
+//
+// A boolean flag is returned as an indicator of whether the subtree rooted
+// at this node was modified as a result of applying the given value.
+func (n *node) merge(version int, value interface{}) (modified bool) {
+	switch mergeValue := value.(type) {
+	case string:
+		// Current node version is newer than the value we are trying to merge
+		if version < n.version {
+			return false
+		}
+
+		// Update the value and clear any existing child nodes
+		n.value, n.version = mergeValue, version
+		if len(n.paths) != 0 {
+			n.paths = make(map[string]*node, 0)
+		}
+		return true
+	case map[string]interface{}:
+		// Skip value if the map is empty
+		if len(mergeValue) == 0 {
+			return false
+		}
+
+		// If we have reached a leaf node we need to compare versions
+		// to decide if we will overwrite the value with the map contents
+		if n.isLeaf() {
+			if version < n.version {
+				return false
+			}
+			n.value = ""
+		}
+
+		// Ensure that this node tracks the latest version
+		if version > n.version {
+			n.version = version
+		}
+
+		// recursively merge the map values and create missing nodes
+		modified := false
+		var subPathNode *node
+		for k, v := range mergeValue {
+			subPathNode = n.paths[k]
+			if subPathNode == nil {
+				subPathNode = makeNode(k, n.depth+1, n)
+			}
+			modified = subPathNode.merge(version, v) || modified
+		}
+
+		return modified
+	}
+
+	panic(fmt.Errorf(`merge() only supports values of type "string" or map[string]interface{}; got %q for key %q`, reflect.TypeOf(value), n.path()))
+}
+
+// Initialize a new config node and attach it to its parent's subPath map.
+func makeNode(segment string, depth int, parent *node) *node {
+	node := &node{
+		segment: segment,
+		depth:   depth,
+		paths:   make(map[string]*node, 0),
+		parent:  parent,
+	}
+
+	if parent != nil {
+		parent.paths[segment] = node
+	}
+
+	return node
+}
---
+++ config/store/node_test.go
@@ -0,0 +1,268 @@
+package store
+
+import (
+	"encoding/json"
+	"reflect"
+	"testing"
+)
+
+func TestNodeLeafValues(t *testing.T) {
+	root := makeNode("", 0, nil)
+	node1 := makeNode("key1", 1, root)
+	node2 := makeNode("key2", 2, node1)
+	node2.value = "2"
+	node3 := makeNode("key3", 2, node1)
+	node4 := makeNode("key4", 3, node3)
+	node4.value = "4"
+
+	specs := []struct {
+		prefix string
+		expMap map[string]string
+	}{
+		{
+			prefix: "",
+			expMap: map[string]string{
+				"key1/key2":      "2",
+				"key1/key3/key4": "4",
+			},
+		},
+		{
+			prefix: "/path/to/key/",
+			expMap: map[string]string{
+				"/path/to/key/key1/key2":      "2",
+				"/path/to/key/key1/key3/key4": "4",
+			},
+		},
+		{
+			prefix: "/path/to/key",
+			expMap: map[string]string{
+				"/path/to/key/key1/key2":      "2",
+				"/path/to/key/key1/key3/key4": "4",
+			},
+		},
+	}
+
+	for specIndex, spec := range specs {
+		valueMap := root.leafValues(spec.prefix, true)
+		if !reflect.DeepEqual(valueMap, spec.expMap) {
+			t.Errorf("[spec %d] expected value map:\n%v\n\ngot:\n%v", specIndex, spec.expMap, valueMap)
+		}
+	}
+}
+
+func TestNodeLeafValuesOnSubPath(t *testing.T) {
+	root := makeNode("", 0, nil)
+	node1 := makeNode("key1", 1, root)
+	node2 := makeNode("key2", 2, node1)
+	node2.value = "2"
+	node3 := makeNode("key3", 2, node1)
+	node4 := makeNode("key4", 3, node3)
+	node4.value = "4"
+
+	specs := []struct {
+		prefix string
+		expMap map[string]string
+	}{
+		{
+			prefix: "",
+			expMap: map[string]string{
+				"key2":      "2",
+				"key3/key4": "4",
+			},
+		},
+		{
+			prefix: "/path/to/key/",
+			expMap: map[string]string{
+				"/path/to/key/key2":      "2",
+				"/path/to/key/key3/key4": "4",
+			},
+		},
+		{
+			prefix: "/path/to/key",
+			expMap: map[string]string{
+				"/path/to/key/key2":      "2",
+				"/path/to/key/key3/key4": "4",
+			},
+		},
+	}
+
+	for specIndex, spec := range specs {
+		valueMap := node1.leafValues(spec.prefix, true)
+		if !reflect.DeepEqual(valueMap, spec.expMap) {
+			t.Errorf("[spec %d] expected value map:\n%v\n\ngot:\n%v", specIndex, spec.expMap, valueMap)
+		}
+	}
+}
+
+func TestNodeSegmentAndPath(t *testing.T) {
+	root := makeNode("", 0, nil)
+	node1 := makeNode("key1", 1, root)
+	node2 := makeNode("key2", 2, node1)
+	node2.value = "2"
+	node3 := makeNode("key3", 2, node1)
+	node4 := makeNode("key4", 3, node3)
+	node4.value = "4"
+
+	specs := []struct {
+		node       *node
+		expSegment string
+		expPath    string
+	}{
+		{
+			node:       root,
+			expSegment: "",
+			expPath:    "/",
+		},
+		{
+			node:       node1,
+			expSegment: "key1",
+			expPath:    "/key1",
+		},
+		{
+			node:       node2,
+			expSegment: "key2",
+			expPath:    "/key1/key2",
+		},
+		{
+			node:       node3,
+			expSegment: "key3",
+			expPath:    "/key1/key3",
+		},
+		{
+			node:       node4,
+			expSegment: "key4",
+			expPath:    "/key1/key3/key4",
+		},
+	}
+
+	for specIndex, spec := range specs {
+		if spec.node.segment != spec.expSegment {
+			t.Errorf("[spec %d] expected segment to be %q; got %q", specIndex, spec.expSegment, spec.node.segment)
+		}
+
+		path := spec.node.path()
+		if path != spec.expPath {
+			t.Errorf("[spec %d] expected path to be %q; got %q", specIndex, spec.expPath, path)
+		}
+	}
+}
+
+func TestNodeMerge(t *testing.T) {
+	specs := []struct {
+		cfg         string
+		version     int
+		expValues   map[string]string
+		expModified bool
+	}{
+		{
+			cfg:     `{"key1":{"key2":"2","key3":{"key4":"4"}}}`,
+			version: 2,
+			expValues: map[string]string{
+				"key1/key2":      "2",
+				"key1/key3/key4": "4",
+			},
+			expModified: true,
+		},
+		{
+			cfg:     `{"key1":{"key2":"new","key3":{"key4":"new"}}}`,
+			version: 1,
+			expValues: map[string]string{
+				"key1/key2":      "2",
+				"key1/key3/key4": "4",
+			},
+			expModified: false,
+		},
+		{
+			cfg:     `{"key1":{"key2":"2","key3":{"key4":"new","key5":"5"}}}`,
+			version: 1,
+			expValues: map[string]string{
+				"key1/key2":      "2",
+				"key1/key3/key4": "4",
+				"key1/key3/key5": "5",
+			},
+			expModified: true,
+		},
+		{
+			cfg:     `{"key1":{"key2":"2","key3":"overwrite!"}}`,
+			version: 10,
+			expValues: map[string]string{
+				"key1/key2": "2",
+				"key1/key3": "overwrite!",
+			},
+			expModified: true,
+		},
+		{
+			cfg:     `{"key1":{"key2":"2","key3":{"key4":"4","key5":"5"}}}`,
+			version: 9,
+			expValues: map[string]string{
+				"key1/key2": "2",
+				"key1/key3": "overwrite!",
+			},
+			expModified: false,
+		},
+		{
+			cfg:     `{"key1":{"key2":"2","key3":{"key4":"4","key5":"5"}}}`,
+			version: 10,
+			expValues: map[string]string{
+				"key1/key2":      "2",
+				"key1/key3/key4": "4",
+				"key1/key3/key5": "5",
+			},
+			expModified: true,
+		},
+		{
+			cfg:     `{"key1":{"key2":"2","key3":{"key4":"4","key5":{}}}}`,
+			version: 11,
+			expValues: map[string]string{
+				"key1/key2":      "2",
+				"key1/key3/key4": "4",
+				"key1/key3/key5": "5",
+			},
+			expModified: true,
+		},
+	}
+	var cfg map[string]interface{}
+	var err error
+	root := makeNode("", 0, nil)
+
+	for specIndex, spec := range specs {
+		cfg = nil
+		err = json.Unmarshal([]byte(spec.cfg), &cfg)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		modified := root.merge(spec.version, cfg)
+		if modified != spec.expModified {
+			t.Errorf("[spec %d] expected root.merge to return %t; got %t", specIndex, spec.expModified, modified)
+		}
+
+		values := root.leafValues("", true)
+		if !reflect.DeepEqual(values, spec.expValues) {
+			t.Errorf("[spec %d] expected merged tree values to be:\n%v\n\ngot:\n%v", specIndex, spec.expValues, values)
+		}
+	}
+}
+
+func TestNodeMergePanic(t *testing.T) {
+	defer func() {
+		if err := recover(); err != nil {
+			expError := `merge() only supports values of type "string" or map[string]interface{}; got "int" for key "/key1/key2"`
+			if pErr, isError := err.(error); isError {
+				if pErr.Error() == expError {
+					return
+				}
+				t.Fatalf("expected merge to panic with %q; got %q", expError, pErr.Error())
+			}
+		}
+
+		t.Fatalf("expected merge() to panic")
+	}()
+
+	root := makeNode("", 0, nil)
+	root.merge(1, map[string]interface{}{
+		"key1": map[string]interface{}{
+			"key2": 1,
+		},
+	})
+}
