f37175ca428127135fd5600308fe4ae3d754613a
Achilleas Anagnostopoulos <achilleas.imap.mbox@gmail.com>
2016-12-27 20:51:05 +0000

    Define a global configuration store instance for usrv sub-packages

---
+++ config/config.go
@@ -0,0 +1,9 @@
+package config
+
+import "github.com/achilleasa/usrv/config/store"
+
+var (
+	// Store is a global configuration store instance that is used to configure
+	// the various usrv components.
+	Store store.Store
+)
