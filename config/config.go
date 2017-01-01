45b10bec6f4e6d408348d365cfd9d5432ffae095
Achilleas Anagnostopoulos <achilleas.imap.mbox@gmail.com>
2017-01-01 15:52:33 +0000

    Expose SetDefaults helper to push defaults to the global store instance

--- config/config.go
+++ config/config.go
@@ -7,3 +7,10 @@ var (
 	// the various usrv components.
 	Store store.Store
 )
+
+// SetDefaults updates the global store instance with the default values for a
+// particular configuration path.
+func SetDefaults(path string, cfg map[string]string) error {
+	_, err := Store.SetKeys(0, path, cfg)
+	return err
+}
