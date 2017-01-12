874abf9d087180bf822dd6ad3be1bcb905472a56
Achilleas Anagnostopoulos <achilleas.imap.mbox@gmail.com>
2017-01-12 07:36:23 +0000

    Implement RPC server

---
+++ server/server.go
@@ -0,0 +1,307 @@
+package server
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"reflect"
+	"runtime"
+	"sync"
+
+	"github.com/achilleasa/usrv"
+	"github.com/achilleasa/usrv/encoding"
+	"github.com/achilleasa/usrv/transport"
+)
+
+var (
+	// DefaultPanicWriter is a sink where the server's default panic handler
+	// writes its output when a panic is recovered.
+	DefaultPanicWriter io.Writer = os.Stderr
+
+	errServeAlreadyCalled = errors.New("server is already listening for incoming requests")
+)
+
+// A PanicHandler is invoked by the server when a panic is recovered while
+// processing an incoming request.
+type PanicHandler func(error)
+
+// Server implements an RPC server that binds a set of endpoints to a transport
+// instance and invokes the endpoint request handlers to process incoming requests
+//
+// The server uses a codec instance to marshal and unmarshal the request and
+// response objects accepted by the endpoint handlers into the low-level message
+// format used by the attached transport. Unless overriden by the WithCodec config
+// option, the server will invoke usrv.DefaultCodecFactory to fetch a codec instance.
+//
+// Unless overriden with the WithTransport config option, the server will invoke
+// usrv.DefaultTransportFactory to fetch a transport instance.
+//
+// The server automatically recoveres and handling any panics while a request is
+// being handled. A built-in panic handler implementation is used that simply
+// write the error and stack-trace to DefaultPanicWriter. However, the panic handler
+// can be overriden using the WithPanicHandler config option.
+type Server struct {
+	// A mutext protecting access to the server fields.
+	mutex sync.Mutex
+
+	// The transport used by the server.
+	transport transport.Transport
+
+	// A codec instance for handling marshaling/unmarshaling.
+	codec encoding.Codec
+
+	// A function for handling panics inside endpoint handlers.
+	panicHandler PanicHandler
+
+	// The name of the service exposed by the server.
+	serviceName string
+
+	// The set of defined endpoints.
+	endpoints []*Endpoint
+
+	// A channel to signal the server go-routine to shut down.
+	doneChan chan struct{}
+}
+
+// NewServer creates a new server instance for the given service name and applies
+// any supplied server options.
+func NewServer(serviceName string, options ...Option) (*Server, error) {
+	srv := &Server{
+		serviceName: serviceName,
+		endpoints:   make([]*Endpoint, 0),
+	}
+
+	// Apply options
+	var err error
+	for _, opt := range options {
+		err = opt(srv)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// Apply defaults
+	srv.setDefaults()
+
+	return srv, nil
+}
+
+// RegisterEndpoints adds one or more endpoints to the set of endpoints exposed
+// by this server instance. This method will return an error if the supplied
+// endpoint fails validation.
+func (s *Server) RegisterEndpoints(endpoints ...*Endpoint) error {
+	var err error
+	for _, ep := range endpoints {
+		err = ep.validate()
+		if err != nil {
+			return err
+		}
+	}
+
+	s.endpoints = append(s.endpoints, endpoints...)
+	return nil
+}
+
+// Listen registers all endpoints with the server's transport and begins serving
+// incoming requests.
+//
+// Calls to Listen block till the server's Close() method is invoked.
+func (s *Server) Listen() error {
+	s.mutex.Lock()
+
+	// Serve already called
+	if s.doneChan != nil {
+		s.mutex.Unlock()
+		return errServeAlreadyCalled
+	}
+
+	var err error
+	for _, endpoint := range s.endpoints {
+		err = s.transport.Bind(s.serviceName, endpoint.Name, s.generateHandler(endpoint))
+		if err != nil {
+			s.mutex.Unlock()
+			return err
+		}
+	}
+
+	err = s.transport.Dial()
+	if err != nil {
+		s.mutex.Unlock()
+		return err
+	}
+
+	s.doneChan = make(chan struct{}, 0)
+	s.mutex.Unlock()
+
+	// Wait for a stop signal
+	<-s.doneChan
+
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	// Shutdown transport and ack the close request
+	s.transport.Close()
+	s.doneChan <- struct{}{}
+
+	return nil
+}
+
+// Close shuts down a server that is listening for incoming connections. After
+// calling close, any blocked calls to Listen() will be unblocked.
+//
+// Calling Close on a server not listening for incoming requests has no effect.
+func (s *Server) Close() {
+	s.mutex.Lock()
+	// Already closed
+	if s.doneChan == nil {
+		s.mutex.Unlock()
+		return
+	}
+
+	// Signal server to shutdown
+	s.doneChan <- struct{}{}
+	s.mutex.Unlock()
+
+	// Wait for the server to ack the close request
+	<-s.doneChan
+
+	// Cleanup the channel
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	close(s.doneChan)
+	s.doneChan = nil
+
+}
+
+// setDefaults applies default settings for fields not set by a server option.
+func (s *Server) setDefaults() {
+	if s.transport == nil {
+		s.transport = usrv.DefaultTransportFactory()
+	}
+
+	if s.codec == nil {
+		s.codec = usrv.DefaultCodecFactory()
+	}
+
+	if s.panicHandler == nil {
+		s.panicHandler = defaultPanicHandler
+	}
+}
+
+// generateHandler generates a handler for an endpoint which can be passed to a
+// transport's Bind call. The handler executes any defined middleware, applies
+// marshaling and unmarshaling via the server's codec and then invokes the application
+// defined endpoint handler. It also registers hooks to recover panics and
+// pass them to the registered panic handler.
+//
+// This method assumes that the endpoint has been properly validated.
+func (s *Server) generateHandler(ep *Endpoint) transport.Handler {
+	handlerType := reflect.TypeOf(ep.Handler)
+	handlerFn := reflect.ValueOf(ep.Handler)
+
+	// Analyze the endpoint handler request and response arguments and
+	// generate an unmarshaler for the request and a marshaler for the response.
+	reqType := handlerType.In(1).Elem()
+	resType := handlerType.In(2).Elem()
+
+	resMarshaler := s.codec.Marshaler()
+	reqUnmarshaler := s.codec.Unmarshaler()
+
+	reqPool := sync.Pool{
+		New: func() interface{} {
+			return reflect.New(reqType).Interface()
+		},
+	}
+	resPool := sync.Pool{
+		New: func() interface{} {
+			return reflect.New(resType).Interface()
+		},
+	}
+
+	// Generate a middleware that handles marshaling and invocation of the endpoint handler.
+	var middlewareChain Middleware = MiddlewareFunc(func(ctx context.Context, req transport.ImmutableMessage, res transport.Message) {
+		var reqObj, resObj interface{}
+		defer func() {
+			if reqObj != nil {
+				reqPool.Put(reqObj)
+			}
+			if resObj != nil {
+				resPool.Put(resObj)
+			}
+		}()
+
+		// Unmarshal request
+		reqObj = reqPool.Get()
+		defer reqPool.Put(reqObj)
+		reqPayload, _ := req.Payload()
+		err := reqUnmarshaler(reqPayload, reqObj)
+		if err != nil {
+			res.SetPayload(nil, err)
+			return
+		}
+
+		// Execute handler
+		resObj = resPool.Get()
+		retVals := handlerFn.Call(
+			[]reflect.Value{
+				reflect.ValueOf(ctx),
+				reflect.ValueOf(reqObj),
+				reflect.ValueOf(resObj),
+			},
+		)
+		ret := retVals[0].Interface()
+		if ret != nil {
+			res.SetPayload(nil, ret.(error))
+			return
+		}
+
+		res.SetPayload(resMarshaler(resObj))
+	})
+
+	// Call each middleware factory in the endpoint factory list in reverse
+	// order and wrap the generated handler.
+	if ep.Middleware != nil {
+		for index := len(ep.Middleware) - 1; index >= 0; index-- {
+			middlewareChain = ep.Middleware[index](middlewareChain)
+		}
+	}
+
+	// Return a compatible transport handler
+	return transport.HandlerFunc(func(req transport.ImmutableMessage, res transport.Message) {
+		if s.panicHandler != nil {
+			defer func() {
+				if r := recover(); r != nil {
+					// Map recovered value to an error that we can feed to the panic handler
+					var err error
+					switch errVal := r.(type) {
+					case error:
+						err = errVal
+					default:
+						err = errors.New(fmt.Sprint(errVal))
+					}
+					s.panicHandler(err)
+					res.SetPayload(nil, fmt.Errorf("remote endpoint paniced: %s", err))
+				}
+			}()
+		}
+		ctx := context.Background()
+		middlewareChain.Handle(ctx, req, res)
+	})
+}
+
+// defaultPanicHandler implements a PanicHandler that writes its output to
+// DefaultPanicWriter.
+func defaultPanicHandler(err error) {
+	stackBuf := make([]byte, 4096)
+	runtime.Stack(stackBuf, false)
+
+	msg := fmt.Sprintf(
+		"Recovered from panic: %v\n\nStacktrace:\n%v\n",
+		err,
+		string(stackBuf),
+	)
+
+	DefaultPanicWriter.Write([]byte(msg))
+}
---
+++ server/server_test.go
@@ -0,0 +1,494 @@
+package server
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+	"reflect"
+	"strings"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/achilleasa/usrv/encoding"
+	"github.com/achilleasa/usrv/encoding/json"
+	"github.com/achilleasa/usrv/transport"
+	"github.com/achilleasa/usrv/transport/provider"
+)
+
+func TestOptionsThatReturnErrors(t *testing.T) {
+	expError := errors.New("option error")
+	opt := func(_ *Server) error { return expError }
+
+	_, err := NewServer("foo", opt)
+	if err != expError {
+		t.Fatalf("expected to get error %v; got %v", expError, err)
+	}
+}
+
+func TestEndpointValidation(t *testing.T) {
+	type request struct{}
+	type response struct{}
+
+	specs := []struct {
+		handler  interface{}
+		expError error
+	}{
+		{
+			handler:  1337,
+			expError: errEndpointHandlerBadSignature,
+		},
+		{
+			handler:  func() {},
+			expError: errEndpointHandlerBadSignature,
+		},
+		{
+			handler:  func(_, _ interface{}) {},
+			expError: errEndpointHandlerBadSignature,
+		},
+		{
+			handler:  func(_, _, _ interface{}) ([]byte, error) { return nil, nil },
+			expError: errEndpointHandlerBadSignature,
+		},
+		{
+			handler:  func(_, _, _ interface{}) int { return 0 },
+			expError: errEndpointHandlerBadSignature,
+		},
+		{
+			handler:  func(_ int, _, _ interface{}) error { return nil },
+			expError: errEndpointHandlerBadArg0,
+		},
+		{
+			handler:  func(_ context.Context, _ request, _ *response) error { return nil },
+			expError: errEndpointHandlerBadArg1,
+		},
+		{
+			handler:  func(_ context.Context, _ *request, _ int) error { return nil },
+			expError: errEndpointHandlerBadArg2,
+		},
+		{
+			handler:  func(_ context.Context, _ *request, _ *response) error { return nil },
+			expError: nil,
+		},
+	}
+
+	srv, err := NewServer("test")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ep := &Endpoint{}
+	err = srv.RegisterEndpoints(ep)
+	if err != errEndpointHasNoName {
+		t.Fatalf("expected error %q; got %v", errEndpointHasNoName.Error(), err)
+	}
+
+	ep.Name = "test"
+	for specIndex, spec := range specs {
+		ep.Handler = spec.handler
+		err = srv.RegisterEndpoints(ep)
+
+		if (spec.expError == nil && err != nil) || (spec.expError != nil && err != spec.expError) {
+			t.Errorf("[spec %d] expected to get error %v; got %v", specIndex, spec.expError, err)
+		}
+	}
+}
+
+func TestEndpointHandler(t *testing.T) {
+	log := []string{}
+	genMiddleware := func(name string) MiddlewareFactory {
+		return func(next Middleware) Middleware {
+			return MiddlewareFunc(func(ctx context.Context, req transport.ImmutableMessage, res transport.Message) {
+				log = append(log, fmt.Sprintf("enter %s", name))
+				next.Handle(ctx, req, res)
+				log = append(log, fmt.Sprintf("exit %s", name))
+			})
+		}
+	}
+
+	type request struct {
+		Name string `json:"name"`
+	}
+
+	type response struct {
+		Greeting string `json:"greeting"`
+	}
+
+	invocation := 0
+	expError := "handler error"
+	ep := &Endpoint{
+		Name: "test",
+		Handler: func(ctx context.Context, req *request, res *response) error {
+			invocation++
+			switch invocation {
+			case 1:
+				log = append(log, "call handler")
+				res.Greeting = "hello " + req.Name
+				return nil
+			default:
+				return errors.New(expError)
+			}
+		},
+		Middleware: []MiddlewareFactory{
+			genMiddleware("1"),
+			genMiddleware("2"),
+			genMiddleware("3"),
+		},
+	}
+
+	srv, err := NewServer("test")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	codec := json.Codec()
+	handler := srv.generateHandler(ep)
+
+	reqObj := &request{Name: "tester"}
+	resMessage := transport.MakeGenericMessage()
+	reqMessage := transport.MakeGenericMessage()
+	defer func() {
+		reqMessage.Close()
+		resMessage.Close()
+	}()
+	reqMessage.SetPayload(codec.Marshaler()(reqObj))
+	handler.Process(reqMessage, resMessage)
+
+	// Examine log
+	expLog := []string{
+		"enter 1",
+		"enter 2",
+		"enter 3",
+		"call handler",
+		"exit 3",
+		"exit 2",
+		"exit 1",
+	}
+	if !reflect.DeepEqual(log, expLog) {
+		t.Fatalf("expected log entries to be:\n%v\n\ngot:\n%v", expLog, log)
+	}
+
+	// Examine response contents
+	resData, err := resMessage.Payload()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expData := []byte(`{"greeting":"hello tester"}`)
+	if !reflect.DeepEqual(resData, expData) {
+		t.Fatalf("expected encoded response data to be:\n%v\n\ngot:\n%v", expData, resData)
+	}
+
+	// Test error handling
+	handler.Process(reqMessage, resMessage)
+	_, err = resMessage.Payload()
+	if err == nil || err.Error() != expError {
+		t.Fatalf("expected error %q; got %v", expError, err)
+	}
+}
+
+func TestMarshalingErrorHandling(t *testing.T) {
+	type request struct{}
+	type response struct{}
+
+	ep := &Endpoint{
+		Name:    "test",
+		Handler: func(ctx context.Context, req *request, res *response) error { return nil },
+	}
+
+	codec := nopCodec()
+	srv, err := NewServer(
+		"test",
+		WithCodec(codec),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	resMessage := transport.MakeGenericMessage()
+	reqMessage := transport.MakeGenericMessage()
+	defer func() {
+		reqMessage.Close()
+		resMessage.Close()
+	}()
+
+	// Test response marshaling error
+	expError := "marshaler error"
+	codec.marshalerFn = func(_ interface{}) ([]byte, error) {
+		return nil, errors.New(expError)
+	}
+
+	srv.generateHandler(ep).Process(reqMessage, resMessage)
+	_, err = resMessage.Payload()
+	if err == nil || err.Error() != expError {
+		t.Fatalf("expected error %q; got %v", expError, err)
+	}
+
+	// Test request unmarshaling error
+	expError = "unmarshaler error"
+	codec.unmarshalerFn = func(_ []byte, _ interface{}) error {
+		return errors.New(expError)
+	}
+
+	srv.generateHandler(ep).Process(reqMessage, resMessage)
+	_, err = resMessage.Payload()
+	if err == nil || err.Error() != expError {
+		t.Fatalf("expected error %q; got %v", expError, err)
+	}
+}
+
+func TestPanicHandling(t *testing.T) {
+	type request struct{}
+	type response struct{}
+
+	invocation := 0
+	panicErr := errors.New("panic with error")
+	panicStr := "panic with string"
+	ep := &Endpoint{
+		Name: "test",
+		Handler: func(ctx context.Context, req *request, res *response) error {
+			var msg interface{}
+			invocation++
+			switch invocation {
+			case 1:
+				msg = panicStr
+			default:
+				msg = panicErr
+			}
+			panic(msg)
+		},
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+	panicHandler := func(err error) {
+		defer wg.Done()
+
+		switch invocation {
+		case 1:
+			if err.Error() != panicStr {
+				t.Fatalf("expected error to be %q; got %v", panicStr, err)
+			}
+		default:
+			if err != panicErr {
+				t.Fatalf("expected error to be %v; got %v", panicErr, err)
+			}
+		}
+	}
+
+	srv, err := NewServer(
+		"test",
+		WithCodec(nopCodec()),
+		WithPanicHandler(panicHandler),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	handler := srv.generateHandler(ep)
+
+	resMessage := transport.MakeGenericMessage()
+	reqMessage := transport.MakeGenericMessage()
+	defer func() {
+		reqMessage.Close()
+		resMessage.Close()
+	}()
+
+	handler.Process(reqMessage, resMessage)
+	handler.Process(reqMessage, resMessage)
+
+	_, err = resMessage.Payload()
+	expErrMsg := "remote endpoint paniced: " + panicErr.Error()
+	if err == nil || err.Error() != expErrMsg {
+		t.Fatalf("expected to get error %q; got %v", expErrMsg, err)
+	}
+
+	wg.Wait()
+}
+
+func TestDefaultPanicHandler(t *testing.T) {
+	var buf bytes.Buffer
+	origWriter := DefaultPanicWriter
+	defer func() {
+		DefaultPanicWriter = origWriter
+	}()
+	DefaultPanicWriter = &buf
+
+	expError := errors.New("some error")
+	defaultPanicHandler(expError)
+
+	expPrefix := fmt.Sprintf("Recovered from panic: %v", expError)
+	if !strings.HasPrefix(buf.String(), expPrefix) {
+		t.Fatalf("expected panic handler output to contain %q", expPrefix)
+	}
+}
+
+func TestServer(t *testing.T) {
+	type request struct {
+		Name string `json:"name"`
+	}
+
+	type response struct {
+		Greeting string `json:"greeting"`
+	}
+
+	srv, err := NewServer(
+		"test",
+		WithTransport(provider.NewInMemory()),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer srv.Close()
+
+	err = srv.RegisterEndpoints(
+		&Endpoint{
+			Name: "test",
+			Handler: func(ctx context.Context, req *request, res *response) error {
+				res.Greeting = "hello " + req.Name
+				return nil
+			},
+		},
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		err := srv.Listen()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	<-time.After(100 * time.Millisecond)
+
+	reqMessage := transport.MakeGenericMessage()
+	defer func() {
+		reqMessage.Close()
+	}()
+	reqMessage.ReceiverField = srv.serviceName
+	reqMessage.ReceiverEndpointField = srv.endpoints[0].Name
+	reqMessage.SetPayload([]byte(`{"name":"tester"}`), nil)
+
+	resMessage := <-srv.transport.Request(reqMessage)
+
+	// Examine response contents
+	resData, err := resMessage.Payload()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expData := []byte(`{"greeting":"hello tester"}`)
+	if !reflect.DeepEqual(resData, expData) {
+		t.Fatalf("expected encoded response data to be:\n%v\n\ngot:\n%v", expData, resData)
+	}
+
+	// Calling Listen a second time should return an error
+	err = srv.Listen()
+	if err != errServeAlreadyCalled {
+		t.Fatalf("expected second call to Serve to return %v; got %v", errServeAlreadyCalled, err)
+	}
+
+	// Signal server to exit
+	srv.Close()
+	wg.Wait()
+}
+
+func TestListenErrors(t *testing.T) {
+	type request struct {
+	}
+
+	type response struct {
+	}
+
+	srv, err := NewServer(
+		"test",
+		WithTransport(provider.NewInMemory()),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer srv.Close()
+
+	// Transport.Bind() returns an error
+	err = srv.RegisterEndpoints(
+		&Endpoint{
+			Name: "test",
+			Handler: func(ctx context.Context, req *request, res *response) error {
+				return nil
+			},
+		},
+		&Endpoint{
+			Name: "test",
+			Handler: func(ctx context.Context, req *request, res *response) error {
+				return nil
+			},
+		},
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = srv.Listen()
+	expErrorMsg := `binding "test/test" already defined`
+	if err == nil || (err != nil && err.Error() != expErrorMsg) {
+		t.Fatalf("expected error %q; got %v", expErrorMsg, err)
+	}
+
+	// Transport.Dial() returns an error
+	srv.transport = &testTransportThatFailsDialing{}
+	srv.endpoints = []*Endpoint{}
+	err = srv.Listen()
+	expError := transport.ErrTransportAlreadyDialed
+	if err != expError {
+		t.Fatalf("expected error %q; got %v", expError, err)
+	}
+}
+
+type testCodec struct {
+	marshalerFn   encoding.Marshaler
+	unmarshalerFn encoding.Unmarshaler
+}
+
+func (c *testCodec) Marshaler() encoding.Marshaler {
+	return c.marshalerFn
+}
+
+func (c *testCodec) Unmarshaler() encoding.Unmarshaler {
+	return c.unmarshalerFn
+}
+
+func nopCodec() *testCodec {
+	return &testCodec{
+		marshalerFn: func(_ interface{}) ([]byte, error) {
+			return nil, nil
+		},
+		unmarshalerFn: func(_ []byte, _ interface{}) error {
+			return nil
+		},
+	}
+}
+
+type testTransportThatFailsDialing struct {
+}
+
+func (tr *testTransportThatFailsDialing) Close() error {
+	return nil
+}
+
+func (tr *testTransportThatFailsDialing) Request(_ transport.Message) <-chan transport.ImmutableMessage {
+	return nil
+}
+
+func (tr *testTransportThatFailsDialing) Bind(_, _ string, _ transport.Handler) error {
+	return nil
+}
+
+func (tr *testTransportThatFailsDialing) Dial() error {
+	return transport.ErrTransportAlreadyDialed
+}
