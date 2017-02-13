a91a345616eb1ff3fac1c3fbf0734f20c7caaaee
Achilleas Anagnostopoulos <achilleas.imap.mbox@gmail.com>
2017-02-13 07:55:28 +0000

    Implement protobuf codec

---
+++ encoding/protobuf/protobuf.go
@@ -0,0 +1,42 @@
+// Package protobuf provides a codec for encoding and decoding of data using protocol buffers.
+package protobuf
+
+import (
+	"errors"
+
+	"github.com/achilleasa/usrv/encoding"
+	"github.com/golang/protobuf/proto"
+)
+
+var (
+	errSrcNotProtobuf = errors.New("marshal target does not implement proto.Message")
+	errDstNotProtobuf = errors.New("unmarshal target does not implement proto.Message")
+)
+
+type protobufCodec struct{}
+
+func (c *protobufCodec) Marshaler() encoding.Marshaler {
+	return func(value interface{}) ([]byte, error) {
+		srcMsg, valid := value.(proto.Message)
+		if !valid {
+			return nil, errSrcNotProtobuf
+		}
+
+		return proto.Marshal(srcMsg)
+	}
+}
+
+func (c *protobufCodec) Unmarshaler() encoding.Unmarshaler {
+	return func(data []byte, target interface{}) error {
+		dstMsg, valid := target.(proto.Message)
+		if !valid {
+			return errDstNotProtobuf
+		}
+		return proto.Unmarshal(data, dstMsg)
+	}
+}
+
+// Codec retuns a codec that implements encoding and decoding of protocol buffers.
+func Codec() encoding.Codec {
+	return &protobufCodec{}
+}
